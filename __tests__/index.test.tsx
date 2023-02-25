c0b0ce24d043f3a6d23a5a96283cc82c94882670
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 18:49:31 +0100

    - add integration test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Home from "@/pages/index";
 
 describe("Home", () => {
@@ -8,5 +9,19 @@ describe("Home", () => {
     const heading = screen.getByText("This is the new home page");
 
     expect(heading).toBeInTheDocument();
+  });
+
+  it("performs integration testing for all components in Home", async () => {
+    render(<Home />);
+
+    const input = screen.getByPlaceholderText("Enter your number here");
+    expect(input).toBeInTheDocument();
+    expect(input).toBeVisible();
+    await userEvent.type(input, "40");
+    waitFor(() => expect(input).toHaveValue("40"));
+
+    const display = screen.getByText("XL");
+    expect(display).toBeInTheDocument();
+    expect(display).toBeVisible();
   });
 });
