87f077b7b6a87e438728524f1c4f1f6b68ce1870
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 12:10:05 +0100

    - writee test for input field

--- components/input/index.test.tsx
+++ components/input/index.test.tsx
@@ -3,5 +3,5 @@ import { InputComponent } from "@/components/input/input";
 
 test("Render Input Component", () => {
   render(<InputComponent />);
-  expect(screen.getByText("This is the input component")).toBeVisible();
+  expect(screen.getByPlaceholderText("Enter your number here")).toBeVisible();
 });
