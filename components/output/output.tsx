585ea936622e32013fa45ff236034fa284927318
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 13:48:11 +0100

    - add interface to output folder - add props to output component

--- components/output/output.tsx
+++ components/output/output.tsx
@@ -1,9 +1,11 @@
-export const OutputComponent = () => {
+import { OutputComponentProps } from "./interface";
+
+export const OutputComponent = ({ numbers }: OutputComponentProps) => {
   return (
     <div className="output">
       <div className="output__wrapper">
         <div className="output__wrapper__panel">
-          <p className="output-panel"></p>
+          <p className="output-panel">{numbers}</p>
         </div>
       </div>
     </div>
