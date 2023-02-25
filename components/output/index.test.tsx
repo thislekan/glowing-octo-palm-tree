acc38ee2dea36eefb5eafe8505e96961ac4b9d9e
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 12:40:07 +0100

    - update output test

--- components/output/index.test.tsx
+++ components/output/index.test.tsx
@@ -4,6 +4,5 @@ import { OutputComponent } from "@/components/output/output";
 test("Render OutputComponent", () => {
   const { container } = render(<OutputComponent />);
   const outputPanel = container.getElementsByClassName("output-panel");
-  expect(screen.getByText("This is the output component")).toBeVisible();
   expect(outputPanel.length).toEqual(1);
 });
