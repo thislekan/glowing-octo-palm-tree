81e48ffb3f560b7e1b2bc2631c50fc6f3ee075ea
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 16:07:31 +0100

    - add constants file to utils folder - add interface to utils folder

---
+++ utils/interface.d.ts
@@ -0,0 +1,17 @@
+export interface NumeralsMapType {
+  M: number;
+  CM: number;
+  D: number;
+  CD: number;
+  C: number;
+  XC: number;
+  L: number;
+  XL: number;
+  X: number;
+  IX: number;
+  V: number;
+  IV: number;
+  I: number;
+}
+
+export type OnlyKeys = keyof NumeralsMapType;
