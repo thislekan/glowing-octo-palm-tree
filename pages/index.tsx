5e1ac33a12fe6a6081caed299c7d77507f521c62
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 23:40:50 +0100

    - import styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,20 @@ import Head from "next/head";
 import { useState } from "react";
 import { OutputComponent } from "@/components/output/output";
 import { InputComponent } from "@/components/input/input";
-
-// import styles from "@/pages/index.module.css";
+import styles from "@/pages/index.module.scss";
 
 export default function Home() {
   const [numbers, setNumbers] = useState("");
 
   return (
-    <div className="home">
+    <div className={styles.home}>
       <Head>
         <title>Number to Roman Numerals Converter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="home__wrapper">
-        <div className="home__wrapper__content">
+      <div className={styles.home__wrapper}>
+        <div className={styles.home__wrapper__content}>
           <h1>Convert numbers to roman numerals</h1>
           <OutputComponent numbers={numbers} />
           <InputComponent numbers={numbers} setNumbers={setNumbers} />
