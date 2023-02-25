38172d41e72d1927343d1349734650ba8f36d68e
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 13:49:56 +0100

    - declare & pass down states as props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from "next/head";
+import { useState } from "react";
+import { OutputComponent } from "@/components/output/output";
+import { InputComponent } from "@/components/input/input";
 
 // import styles from "@/pages/index.module.css";
 
 export default function Home() {
+  const [numbers, setNumbers] = useState("");
+
   return (
     <div className="home">
       <Head>
@@ -13,6 +18,8 @@ export default function Home() {
       <div className="home__wrapper">
         <div className="home__wrapper__content">
           <h1>This is the new home page</h1>
+          <OutputComponent numbers={numbers} />
+          <InputComponent numbers={numbers} setNumbers={setNumbers} />
         </div>
       </div>
     </div>
