4154bae63a822f9f69bc6ff501663eb66f07cb5f
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 16:08:55 +0100

    - add additional test to cater for numbers conversion display - chnage name of inital test written

--- components/output/index.test.tsx
+++ components/output/index.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { OutputComponent } from "@/components/output/output";
 
-test("Render OutputComponent", () => {
-  const { container } = render(<OutputComponent />);
+test("Render OutputComponent without props", () => {
+  const { container } = render(<OutputComponent numbers="" />);
   const outputPanel = container.getElementsByClassName("output-panel");
   expect(outputPanel.length).toEqual(1);
+});
+
+test("Render OutputComponent with Props", () => {
+  render(<OutputComponent numbers="19" />);
+  expect(screen.getByText("XIX")).toBeVisible();
+  expect(screen.getAllByText("XIX").length).toBe(1);
 });
