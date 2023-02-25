6d3c19d59c03d85ef339da41d2923b187942e1c8
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 16:58:54 +0100

    - import package - DRY for Render Input Component test - test user typing on input component

--- components/input/index.test.tsx
+++ components/input/index.test.tsx
@@ -1,7 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { InputComponent } from "@/components/input/input";
 
 test("Render Input Component", () => {
-  render(<InputComponent />);
-  expect(screen.getByPlaceholderText("Enter your number here")).toBeVisible();
+  const mockFn = jest.fn();
+  render(<InputComponent setNumbers={mockFn} numbers="" />);
+  const input = screen.getByPlaceholderText("Enter your number here");
+  expect(input).toBeVisible();
+  expect(input).toHaveDisplayValue("");
+});
+
+test("Typing activity on Input Component", async () => {
+  const mockFn = jest.fn();
+  render(<InputComponent setNumbers={mockFn} numbers="" />);
+  const input = screen.getByPlaceholderText("Enter your number here");
+  expect(input).toBeVisible();
+  expect(input).toHaveTextContent("");
+  await userEvent.type(input, "15");
+  expect(mockFn).toHaveBeenCalled();
+  expect(mockFn).toHaveBeenCalledTimes(2);
 });
