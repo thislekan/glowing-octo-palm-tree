b0f5dbf0785e97e2ecb55df8cd26315a754f7535
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 12:37:31 +0100

    - test for output panel in output component

--- components/output/index.test.tsx
+++ components/output/index.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import { OutputComponent } from "@/components/output/output";
 
 test("Render OutputComponent", () => {
-  render(<OutputComponent />);
+  const { container } = render(<OutputComponent />);
+  const outputPanel = container.getElementsByClassName("output-panel");
   expect(screen.getByText("This is the output component")).toBeVisible();
+  expect(outputPanel.length).toEqual(1);
 });
