b39a3551a8eb165b07ec05dc6a46704f46bbccdd
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2023-02-25 17:47:22 +0100

    - improve DRY - add test for letters input

--- components/input/index.test.tsx
+++ components/input/index.test.tsx
@@ -2,21 +2,32 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { InputComponent } from "@/components/input/input";
 
-test("Render Input Component", () => {
+interface SetupParams {
+  numbers?: string;
+}
+const setup = ({ numbers }: SetupParams) => {
   const mockFn = jest.fn();
-  render(<InputComponent setNumbers={mockFn} numbers="" />);
+  render(<InputComponent setNumbers={mockFn} numbers={numbers || ""} />);
   const input = screen.getByPlaceholderText("Enter your number here");
+
+  return { input, mockFn };
+};
+
+test("Render Input Component", () => {
+  const { input } = setup({});
   expect(input).toBeVisible();
   expect(input).toHaveDisplayValue("");
 });
 
 test("Typing activity on Input Component", async () => {
-  const mockFn = jest.fn();
-  render(<InputComponent setNumbers={mockFn} numbers="" />);
-  const input = screen.getByPlaceholderText("Enter your number here");
-  expect(input).toBeVisible();
-  expect(input).toHaveTextContent("");
+  const { input, mockFn } = setup({});
   await userEvent.type(input, "15");
   expect(mockFn).toHaveBeenCalled();
   expect(mockFn).toHaveBeenCalledTimes(2);
+});
+
+test("Typing letters on Input Component", async () => {
+  const { input, mockFn } = setup({ numbers: "" });
+  await userEvent.type(input, "returned");
+  expect(mockFn).not.toHaveBeenCalled();
 });
